69b3f3a2eff7431b0862ac6455de04173b570425
arjan-spieard@arcor.de <arjan-spieard@arcor.de@7a5f31bc-d970-93e3-ec9a-557ea78d8f60>
2011-09-09 21:18:59 +0000

    dit is de hard-code van mijn menu-tje kijk maar even ik heb verder niet aangepast op alleen het menu zal ik morgen als ik eraan toe kom mee bezig gaan (:

---
+++ trunk/menu/index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" > 
+<head>
+<script type="text/JavaScript">
+//courtesy of BoogieJack.com
+function killCopy(e){
+return false
+}
+function reEnable(){
+return true
+}
+document.onselectstart=new Function ("return false")
+if (window.sidebar){
+document.onmousedown=killCopy
+document.onclick=reEnable
+}
+</script>
+
+<jdoc:include type="head" /> 
+<link rel="stylesheet" href="file:///C:/Documents and Settings/Windows/Bureaublad/Template/css/template.css" type="text/css" /> 
+</head> 
+<body>
+<div id="container">  
+    <div id="header"><jdoc:include type="modules" name="top" /><center><a href="http://www.rs4ever.vechtdal-ict.nl"><img src="templates/tutorial_template/images/Banner.jpg" width="1050" height="80" /></a></center></div>    
+    <div id="sidebar_left" class="float"><jdoc:include type="modules" name="left" />
+
+<div id="menu">
+<ul>
+  <li>
+    <ul>
+      <li><a href="file:///C:/Documents and Settings/Windows/Bureaublad/Template/index.php">Home</a></li>
+      <li><a href="file:///C:/Documents and Settings/Windows/Bureaublad/Template/Artikelen/Voorwoord.html">Voorwoord</a></li>
+      <li><a href="file:///C:/Documents and Settings/Windows/Bureaublad/Template/Artikelen/Hoofd- en Deelvragen.html">Hoofd- en Deelvragen</a>
+        <ul>
+          <li><a href="file:///C:/Documents and Settings/Windows/Bureaublad/Template/Artikelen/Theoretische deelvragen.html">Theoretische deelvragen</a>
+	    <ul>
+	      <li><a href="file:///C:/Documents and Settings/Windows/Bureaublad/Template/Artikelen/Wat is een coilgun en hoe werkt hij?.html">Wat is een coilgun en hoe werkt hij?</a></li>
+	      <li><a href="file:///C:/Documents and Settings/Windows/Bureaublad/Template/Artikelen/Wat is het nut de coilgun in de dagelijkse maatschappij?.html">Wat is het nut de coilgun in de dagelijkse maatschappij?</a></li>
+	      <li><a href="file:///C:/Documents and Settings/Windows/Bureaublad/Template/Artikelen/Bestaat er een verband tussen het aantal volts en de snelheid en kracht van het projectiel?">Bestaat er een verband tussen het aantal volts en de snelheid en kracht van het projectiel?</a></li>
+	      <li><a href="file:///C:/Documents and Settings/Windows/Bureaublad/Template/Artikelen/Zijn er verschillende soorten coilguns?.html">Zijn er verschillende soorten coilguns?.html</a></li>
+	      <li><a href="file:///C:/Documents and Settings/Windows/Bureaublad/Template/Artikelen/Wat is in theorie een goede legering voor de munitie van de coilgun?.html">Wat is in theorie een goede legering voor de munitie van de coilgun?</a></li>
+	    </ul>
+	  </li>
+          <li><a href="file:///C:/Documents and Settings/Windows/Bureaublad/Template/Artikelen/Praktische deelvragen.html">Praktische deelvragen</a>
+	    <ul>
+	      <li><a href="file:///C:/Documents and Settings/Windows/Bureaublad/Template/Artikelen/Is het mogelijk om een goed functionerende coilgun te bouwen die op normale 230 volt van het lichtnet werkt?.html">Is het mogelijk om een goed functionerende coilgun te bouwen die op normale 230 volt van het lichtnet werkt?</a></li>
+	      <li><a href="file:///C:/Documents and Settings/Windows/Bureaublad/Template/Artikelen/Is er een verband tussen het aantal volts en de afstand die de kogel van de coilgun aflegt?.html">Is er een verband tussen het aantal volts en de afstand die de kogel van de coilgun aflegt?</a></li>
+	    </ul>
+	  </li>
+
+        </ul>
+      </li>
+      <li><a href="file:///C:/Documents and Settings/Windows/Bureaublad/Template/test disclamer.html">Het bouwproces</a></li>
+      <li><a href="file:///C:/Documents and Settings/Windows/Bureaublad/Template/Artikelen/Het experimenteren.html">Het experimenteren</a></li>
+      <li><a href="file:///C:/Documents and Settings/Windows/Bureaublad/Template/Artikelen/De berekeningen &amp; resultaten.html">De berekeningen &amp; resultaten</a></li>
+      <li><a href="file:///C:/Documents and Settings/Windows/Bureaublad/Template/Artikelen/De grafieken van de resultaten.html">De grafieken van de resultaten</a></li>
+      <li><a href="file:///C:/Documents and Settings/Windows/Bureaublad/Template/Artikelen/Conclusie.html">Conclusie</a></li>
+      <li><a href="file:///C:/Documents and Settings/Windows/Bureaublad/Template/image_slideshow.html">Foto's</a></li>
+    </ul>
+  </li>
+</ul>
+						
+</div>
+
+</div> 	
+    <div id="content" class="float"><jdoc:include type="component" /></div>  
+    <div id="footer" class="clear"><jdoc:include type="modules" name="footer" />
+      <ul id="Footer_menu">
+        <li><a href="http://rs4ever.vechtdal-ict.nl">Home</a></li>
+        <li><a href="http://www.rs4ever.vechtdal-ict.nl/index.php?option=com_content&amp;view&#61;article&amp;id&#61;77">About us</a></li>
+	<li><a href="http://www.rs4ever.vechtdal-ict.nl/index.php?option=com_content&amp;view&#61;article&amp;id&#61;74">Contact us</a></li>
+	<li><a href="/templates/tutorial_template/sitemap.html">Sitemap</a></li>
+	<li><a href="http://www.rs4ever.vechtdal-ict.nl/index.php?option=com_content&amp;view&#61;article&amp;id&#61;75">Terms &amp; conditions</a></li>
+	<li><a href="http://www.rs4ever.vechtdal-ict.nl/index.php?option=com_content&amp;view&#61;article&amp;id&#61;76">Privacy policy</a><br><HR COLOR="#400000"></li>
+    <p>Copyright &copy; 2010 Arjan Spieard. All Rights Reserved.</p>
+      </ul>
+    </div>
+</div>
+</body>  
+</html>
