902c2698ba3f747636752e6117214471cec476ce
hans-jurgenbakkenes@hotmail.com <hans-jurgenbakkenes@hotmail.com@7a5f31bc-d970-93e3-ec9a-557ea78d8f60>
2011-09-30 14:28:42 +0000

    - uploaden van profielfoto is nu mogelijk

---
+++ uploadprofielfoto.php
@@ -0,0 +1,134 @@
+<?php
+/**
+ * @author: Hans-Jurgen Bakkenes
+ * @description: 
+ */
+$pagina = pagina::getInstantie();
+
+$pagina->setTitel("Upload je profiel foto");
+$pagina->setCss("style.css");
+
+echo $pagina->getVereisteHTML();
+?>
+<div id="container">
+	<?php echo $pagina->getHeader(); ?>
+	<div id="page">
+		<?php echo $pagina->getMenu(); ?>
+		<div id="content">
+			<h1><?php echo $pagina->getTitel(); ?></h1>
+			<form action="" method="post" enctype="multipart/form-data">
+				<table>
+					<tr>
+						<td>
+							<input type="file" name="profielfoto" />
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<input type="submit" name="profielfoto" />
+						</td>
+					</tr>
+				</table>
+			</form>
+			<?php
+			database::getInstantie();
+			
+			function checkValideUpload($code)
+			{
+				if ($code == UPLOAD_ERR_OK) {
+					return;
+				}
+
+				switch ($code) {
+					case UPLOAD_ERR_INI_SIZE:
+					case UPLOAD_ERR_FORM_SIZE:
+						$bericht = 'Afbeelding is te groot';
+						break;
+					case UPLOAD_ERR_PARTIAL:
+						$bericht = 'De afbeelding is gedeeltelijk geupload';
+						break;
+					case UPLOAD_ERR_NO_FILE:
+						$bericht = 'Geen afbeelding geupload';
+						break;
+					case UPLOAD_ERR_NO_TMP_DIR:
+						$bericht = 'De upload map is niet gevonden';
+						break;
+					case UPLOAD_ERR_CANT_WRITE:
+						$bericht = 'Kon de afbeelding niet wegschrijven';
+						break;
+					case UPLOAD_ERR_EXTENSION:
+						$bericht = 'Kon afbeelding niet openen door onbekende extensie';
+						break;
+					default:
+						$bericht = 'Onbekende error';
+				}
+				return $bericht;
+			}
+
+			if (isset($_FILES["profielfoto"]["tmp_name"])) { //heeft de foto een tijdelijke naam?
+				$afbeelding = $_FILES['profielfoto'];
+				if (checkValideUpload($afbeelding["error"]) == "") {
+					if (filesize($afbeelding['tmp_name']) < 100000) { //is de afbeelding in de temp map, minder dan 100kb?
+						if (getimagesize($_FILES['profielfoto']['tmp_name'])) { //kunnen we eigenschappen ophalen van de afbeelding?
+							list($width, $height, $type, $attr) = getimagesize($_FILES['profielfoto']['tmp_name']);
+							if ($width <= 100 && $height <= 100) {
+
+								$bestand = false;
+								if (exif_imagetype($_FILES['profielfoto']['tmp_name']) == IMAGETYPE_GIF) {
+									$bestand = true;
+								} else if (exif_imagetype($_FILES['profielfoto']['tmp_name']) == IMAGETYPE_PNG) {
+									$bestand = true;
+								} else if (exif_imagetype($_FILES['profielfoto']['tmp_name']) == IMAGETYPE_JPEG) {
+									$bestand = true;
+								}
+
+								if ($bestand) {
+									$pointer = fopen($afbeelding['tmp_name'], "rb"); //open het bestand in binary format
+									$afbeelding2 = fread($pointer, filesize($afbeelding['tmp_name'])); //geeft een string terug vam het bestand
+									database::getInstantie();
+									$sql = "UPDATE `student` SET `profielfoto`='".mysql_real_escape_string(base64_encode($afbeelding2))."' WHERE `studentid` = 1";
+									mysql_query($sql) or die(mysql_error());
+
+//										if (!file_exists($_FILES['profielfoto']['name'])) {
+//											$uploaddir = $_SERVER["DOCUMENT_ROOT"].'/project/profielfoto/';
+//											$uploadfile = $uploaddir . basename($_FILES['profielfoto']['name']);
+//
+//											if (move_uploaded_file($_FILES['profielfoto']['tmp_name'], $uploadfile)) {
+//												echo "Bestand ". $_FILES['profielfoto']['name'] ." is succesvol geupload.\n";
+//											} else {
+//												echo "Kon de afbeelding niet uploaden\n\r";
+//											}
+//										} else {
+//											echo "Een bestand met deze naam bestaat reeds.";
+//										}
+								} else {
+									echo "Bestand type onjuist";
+								}
+							} else {
+								echo "Afbeelding is te groot.";
+							}
+						} else {
+							echo "Onjuist afbeelding bestand";
+						}
+					} else {
+						echo "Te groot bestand.";
+					}
+				} else {
+					echo checkValideUpload($afbeelding["error"]);
+				}
+			}
+			
+			$sql = "SELECT `profielfoto` FROM `student` WHERE studentid=1";
+			$result = mysql_query($sql);
+			while ($array = mysql_fetch_assoc($result)) {
+				echo "<img src=\"data:image/png;base64,".$array["profielfoto"]."\" alt=\"avatar\" title=\"avatar\" />";
+			}
+			?>
+		</div>
+	</div>
+			<?php echo $pagina->getFooter(); ?>
+</div>
+
+	<?php
+	echo $pagina->getVereisteHTMLafsluiting();
+	?>
