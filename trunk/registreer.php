7162771022c4cf969a91159c0db5ac12821d599f
hans-jurgenbakkenes@hotmail.com <hans-jurgenbakkenes@hotmail.com@7a5f31bc-d970-93e3-ec9a-557ea78d8f60>
2011-09-30 14:28:42 +0000

    - uploaden van profielfoto is nu mogelijk

--- trunk/registreer.php
+++ trunk/registreer.php
@@ -1,16 +1,16 @@
 <?php
-	/**
-	* @author: Kay van Bree
-	* @description: Op deze pagina kunnen mensen zich registreren om lid te worden van de website.
-	*				Er wordt informatie ingevoerd en deze wordt door middel van PHP_SELF op dezelfde
-	*				pagina verwerkt. Met het verwerken wordt er een rij in de tabel Gebruikers aangemaakt.
-	*/
-	
-	class registreer(){
-		public function maakRij($gegevens){
-			/*Deze functie bevat de queries die nodig zijn om een nieuw record te maken in de tabel
-			Gebruikers. Ook worden ze uitgevoerd.*/
-			
-		}
+
+/**
+ * @author: Kay van Bree
+ * @description: Op deze pagina kunnen mensen zich registreren om lid te worden van de website.
+ * 				Er wordt informatie ingevoerd en deze wordt door middel van PHP_SELF op dezelfde
+ * 				pagina verwerkt. Met het verwerken wordt er een rij in de tabel Gebruikers aangemaakt.
+ */
+class registreer() {
+	public function maakRij($gegevens){
+	/* Deze functie bevat de queries die nodig zijn om een nieuw record te maken in de tabel
+	  Gebruikers. Ook worden ze uitgevoerd. */
+		
 	}
+}
 ?>
