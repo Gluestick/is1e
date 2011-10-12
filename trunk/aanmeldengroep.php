feeceaf32e7043cffa85debb3c605744b73f6765
hans-jurgenbakkenes@hotmail.com <hans-jurgenbakkenes@hotmail.com@7a5f31bc-d970-93e3-ec9a-557ea78d8f60>
2011-10-12 20:12:04 +0000

    - w3 compliant fouten gefixed - styles aangepast - aanpassingen op raadpleeg profiel voor gegevens - aanmeldengroep.php wijzigingen

--- trunk/aanmeldengroep.php
+++ trunk/aanmeldengroep.php
@@ -4,8 +4,16 @@
  * @description: 
  */
 $pagina = pagina::getInstantie();
+database::getInstantie();
 
-$pagina->setTitel("Aanmelden voor");
+$sql = "SELECT * FROM groep WHERE groepid = ".mysql_real_escape_string($_GET["groepid"]);
+$resultaat = mysql_query($sql);
+$naam = "";
+if (mysql_num_rows($resultaat) > 0) {
+	$array = mysql_fetch_assoc($resultaat);
+	$naam = " voor de \"".$array["naam"]."\" groep";
+}
+$pagina->setTitel("Aanmelden".$naam);
 $pagina->setCss("style.css");
 
 echo $pagina->getVereisteHTML();
@@ -16,9 +24,37 @@ echo $pagina->getVereisteHTML();
 		<?php echo $pagina->getMenu(); ?>
 		<div id="content">
 			<h1><?php echo $pagina->getTitel(); ?></h1>
-			<p>
-				tekst
-			</p>
+			<?php
+			if (isset($_POST["aanmelden"])) {
+				$sql = "SELECT * FROM groeplid WHERE groepid = ".mysql_real_escape_string($_GET["id"])." AND studentid = ".mysql_real_escape_string($_POST["studentid"]);
+				$result = mysql_query($sql);
+				if (mysql_num_rows()) {
+					
+				}
+			}
+			?>
+			<form method="post" action="aanmeldengroep.php?groepid=<?php echo $_GET["groepid"]; ?>">
+				<table>
+					<tr>
+						<td>
+							<select name="student">
+								<?php
+								$sql = "SELECT studentid, voornaam, achternaam FROM student";
+								$resultaat_van_server = mysql_query($sql);
+								while ($rij = mysql_fetch_assoc($resultaat_van_server)) {
+									echo "<option value=\"".$rij["studentid"]."\">".$rij["voornaam"]." ".$rij["achternaam"]."</option>";
+								}
+								?>
+							</select>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<input type="submit" name="aanmelden" value="aanmelden" />
+						</td>
+					</tr>
+				</table>
+			</form>
 		</div>
 	</div>
 	<?php echo $pagina->getFooter(); ?>
