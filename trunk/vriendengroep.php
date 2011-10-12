3c8a423e847bce8ec839f8f019f740f3831785a1
hans-jurgenbakkenes@hotmail.com <hans-jurgenbakkenes@hotmail.com@7a5f31bc-d970-93e3-ec9a-557ea78d8f60>
2011-10-12 18:11:55 +0000

    - benodigdheden voor groepen

--- trunk/vriendengroep.php
+++ trunk/vriendengroep.php
@@ -6,7 +6,17 @@
 $pagina = pagina::getInstantie();
 database::getInstantie();
 
-$pagina->setTitel("Vriendengroep");
+$naam = "";
+if (isset($_GET["groepid"])) {
+	$sql = "SELECT * FROM groep INNER JOIN student ON eigenaar = studentid WHERE groepid = ".mysql_real_escape_string($_GET["groepid"]." LIMIT 1;");
+	$resultaat_van_server = mysql_query($sql);
+	if (mysql_num_rows($resultaat_van_server) > 0) {
+		$array = mysql_fetch_assoc($resultaat_van_server);
+	}
+	$naam = " ".$array["naam"]." van ".$array["voornaam"]." ".$array["achternaam"];
+}
+
+$pagina->setTitel("Vriendengroep".$naam);
 $pagina->setCss("style.css");
 
 echo $pagina->getVereisteHTML();
@@ -18,25 +28,20 @@ echo $pagina->getVereisteHTML();
 		<div id="content">
 			<h1><?php echo $pagina->getTitel(); ?></h1>
 			<?php
-			if (isset($_GET["id"])) {
-				$sql = "SELECT * FROM groep WHERE eigenaar = ".mysql_real_escape_string($_GET["id"]);
-				$resultaat_van_server = mysql_query($sql);
-				if (mysql_num_rows($resultaat_van_server) > 0) {
-					echo "<table style=\"text-align:left;\">";
-					while ($array = mysql_fetch_assoc($resultaat_van_server)) {
-						$query = "SELECT * FROM groeplid INNER JOIN student ON student.studentid = groeplid.studentid WHERE groepid = ".$array["groepid"];
-						$resultaat = mysql_query($query);
-						echo "<tr><td valign=\"middle\" rowspan=\"".(mysql_num_rows($resultaat) + 1)."\">".$array["naam"]."</td></tr>";
-						while ($rij = mysql_fetch_assoc($resultaat)) {
-							echo "<tr><td><a href=\"raadplegenprofiel.php?id=".$rij["studentid"]."\">".$rij["voornaam"]." ".$rij["achternaam"]."</a></td></tr>";
-						}
+			if (isset($_GET["groepid"])) {
+				echo "<table style=\"text-align:left;\">";
+				echo "<tr><th colspan=\"4\">Leden:</th></tr>";
+				echo "<tr><th>Studentennummer</th><th>Naam</th><th>Geslacht</th><th>Geboortedatum</th></tr>";
+
+					$query = "SELECT * FROM groeplid INNER JOIN student ON student.studentid = groeplid.studentid WHERE groepid = ".$array["groepid"];
+					$resultaat = mysql_query($query);
+					while ($rij = mysql_fetch_assoc($resultaat)) {
+						echo "<tr><td>".$rij["studentnr"]."</td><td><a href=\"raadplegenprofiel.php?id=".$rij["studentid"]."\">".$rij["voornaam"]." ".$rij["achternaam"]."</a></td><td>".$rij["geslacht"]."</td><td>".tijd::formatteerTijd($rij["geboortedatum"], "d-m-Y")."</td></tr>";
 					}
-					echo "</table>";
-				} else {
-					echo "U heeft nog geen groepen aangemaakt";
-				}
+				echo "</table>";
+				echo "<a href=\"aanmeldengroep.php?groepid=".$array["groepid"]."\">Voor groep aanmelden</a>";
 			} else {
-				echo "Geen gegevens beschikbaar";
+				echo "Geen resultaten beschikbaar.";
 			}
 			?>
 		</div>
