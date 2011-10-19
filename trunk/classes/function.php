059c89d5acaaac3bee37cafd312217efb4453f27
hans-jurgenbakkenes@hotmail.com <hans-jurgenbakkenes@hotmail.com@7a5f31bc-d970-93e3-ec9a-557ea78d8f60>
2011-10-19 14:10:36 +0000

    - kleine aanpassingen

--- trunk/classes/function.php
+++ trunk/classes/function.php
@@ -1,48 +1,57 @@
 <?php
-	/**
-	 * Losse functies
-	 * 
-	 * @author: Kay van Bree
-	 */
-	 
-	 /**
-	  * Returns true als er iemand in is gelogd.
-	  * @return type 
-	  */
-	 function isMember(){
-		if(isset($_SESSION['login']) && $_SESSION['login'] == true){
-			 return true;
-		} else {
-			return false;
-		}
+
+/**
+ * Losse functies
+ * 
+ * @author: Kay van Bree
+ */
+
+/**
+ * Returns true als er iemand in is gelogd.
+ * @return boolean
+ */
+function isMember()
+{
+	if (isset($_SESSION['login']) && $_SESSION['login'] == true) {
+		return true;
+	} else {
+		return false;
 	}
-	
-	/**
-	 * Returns true als de gebruiker admin is.
-	 * @return type 
-	 */
-	function isAdmin(){
-		if(isMember() && $_SESSION['role'] == 1){
-			return true;
-		} else {
-			return false;
-		}
+}
+
+/**
+ * Returns true als de gebruiker admin is.
+ * @return boolean 
+ */
+function isAdmin()
+{
+	if (isMember() && $_SESSION['role'] == 1) {
+		return true;
+	} else {
+		return false;
 	}
-	
-	/**
-	 * Returns true als de gebruiker een student is.
-	 */
-	function isStudent(){
-		if(isMember() && isset($_SESSION['studennr'])){
-			return true;
-		} else {
-			return false;
-		}
+}
+
+/**
+ * Returns true als de gebruiker een student is.
+ */
+function isStudent()
+{
+	if (isMember() && isset($_SESSION['studennr'])) {
+		return true;
+	} else {
+		return false;
 	}
-	
-	function isHimSelf(){
-		if($_GET['id'] != $_SESSION['user_id']){
-			header('Location: index.php');
-		}
+}
+
+/**
+ * Controleert of iemand de gegevens
+ */
+function isHimSelf()
+{
+	if ($_GET['id'] != $_SESSION['user_id']) {
+		header('Location: index.php');
 	}
+}
+
 ?>
