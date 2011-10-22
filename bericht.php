499c9b6e9c89d57fe11198f2b5a502b945fa9d89
hans-jurgenbakkenes@hotmail.com <hans-jurgenbakkenes@hotmail.com@7a5f31bc-d970-93e3-ec9a-557ea78d8f60>
2011-10-22 22:02:29 +0000

    - improvements

--- bericht.php
+++ bericht.php
@@ -10,15 +10,39 @@ if (isset($_POST["informatie"])) {
 				
 				
 				if ($info[0] == "bericht") {
-					$query = "SELECT * FROM ".$info[0]." WHERE ".$info[0]."id = ".$info[1].";";
+					$query = "SELECT * FROM bericht WHERE berichtid = ".$info[1].";";
 					$resultaat_van_server = mysql_query($query);
 					$array = mysql_fetch_assoc($resultaat_van_server);
-					
+					if (!is_null($array["groepid"])) {
+						
+					} else if (!is_null($array["naar"])) {
+						
+					}
 				} else if ($info[0] == "reactie") {
-					$query = "SELECT * FROM reactie INNER JOIN student ON afzender_id = student.studentid WHERE ".$info[0]."id = ".$info[1].";";
+					$query = "SELECT * FROM reactie INNER JOIN evenement ON reactie.evenementid = evenement.evenementid INNER JOIN student ON afzender_id = student.studentid WHERE reactieid = ".$info[1].";";
 					$resultaat_van_server = mysql_query($query);
 					$array = mysql_fetch_assoc($resultaat_van_server);
-					
+					?>
+					<table cellpadding="0" cellspacing="0">
+						<tr class="emailheader">
+							<th>Van:</th><td><?php echo $array["voornaam"]." ".$array["achternaam"]; ?></td>
+						</tr>
+						<tr class="emailheader">
+							<th>Geplaatst:</th><td><?php echo tijd::vervangEngelsDoorNederlands(tijd::formatteerTijd($array["tijdstip"], "l j F Y")); ?></td>
+						</tr>
+						<tr class="emailheader last">
+							<th>Bericht bij evenement:</th><td><?php echo $array["naam"]; ?></td>
+						</tr>
+						<tr>
+							<td colspan="2" style="padding:14px 3px;">
+								<?php echo nl2br($array["inhoud"]); ?>
+								<br />
+								<br />
+								<a href="evenement.php?id=<?php echo $array["evenementid"]; ?>"><?php echo "Klik hier om naar evenement \"".$array["naam"]."\" te gaan."; ?></a>
+							</td>
+						</tr>
+					</table>
+					<?php
 				} else if ($info[0] == "profielbericht") {
 					$query = "SELECT *,profielbericht.studentid as aan, CONCAT(O.voornaam,' ',O.achternaam) as ontvanger FROM profielbericht INNER JOIN student as O ON O.studentid = profielbericht.studentid INNER JOIN student as A ON afzender = A.studentid WHERE profielberichtid = ".$info[1].";";
 					$resultaat_van_server = mysql_query($query);
@@ -32,11 +56,14 @@ if (isset($_POST["informatie"])) {
 							<th>Geplaatst:</th><td><?php echo tijd::vervangEngelsDoorNederlands(tijd::formatteerTijd($array["datum"], "l j F Y")); ?></td>
 						</tr>
 						<tr class="emailheader last">
-							<th>Aan:</th><td><a href="raadplegenprofiel.php?id=<?php echo $array["aan"]; ?>"><?php echo $array["ontvanger"]; ?></a></td>
+							<th>Profielbericht aan:</th><td><?php echo $array["ontvanger"]; ?></td>
 						</tr>
 						<tr>
 							<td colspan="2" style="padding:14px 3px;">
 								<?php echo nl2br($array["inhoud"]); ?>
+								<br />
+								<br />
+								<a href="raadplegenprofiel.php?id=<?php echo $array["aan"]; ?>"><?php echo "Klik hier om naar het profiel van \"".$array["ontvanger"]."\" te gaan."; ?></a>
 							</td>
 						</tr>
 					</table>
