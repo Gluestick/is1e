7d5527b046a095ed5329ee81dfe14b20a4da5c03
hans-jurgenbakkenes@hotmail.com <hans-jurgenbakkenes@hotmail.com@7a5f31bc-d970-93e3-ec9a-557ea78d8f60>
2011-10-22 20:29:04 +0000

    - profielberichten doen het in de inbox

--- classes/tijd.php
+++ classes/tijd.php
@@ -24,6 +24,24 @@ class tijd {
 		$datum = new DateTime($tijd);
 		return $datum->format($format);
 	}
+	
+	/**
+	 * Deze functie vervangt bijv. monday door maandag en
+	 * august door augustus
+	 * @param string $datum
+	 * @return string
+	 */
+	public static function vervangEngelsDoorNederlands($datum) {
+		$dagenNL = array("maandag","dinsdag","woensdag","donderdag","vrijdag","zaterdag","zondag");
+		$dagenEN = array("monday","tuesday","wednesday","thursday","friday","saturday","sunday");
+		$maandenNL = array("januari","februari","maart","april","mei","juni","juli","augustus","september","oktober","november","december");
+		$maandenEN = array("januari","februari","march","april","may","june","july","august","september","october","november","december");
+		
+		$tekst = str_ireplace($dagenEN, $dagenNL, $datum);
+		$tekst = str_ireplace($maandenEN, $maandenNL, $tekst);
+		
+		return $tekst;
+	}
 }
 
 ?>
