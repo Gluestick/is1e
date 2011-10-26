0fc722180440ae4bd9856230019c7e1d154bbdcf
hans-jurgenbakkenes@hotmail.com <hans-jurgenbakkenes@hotmail.com@7a5f31bc-d970-93e3-ec9a-557ea78d8f60>
2011-10-26 19:29:04 +0000

    - kleine aanpassing

--- wijzigevenement.php
+++ wijzigevenement.php
@@ -3,15 +3,70 @@
  * @author: Kay van Bree, Kajel Bhikhoe
  * @description: 
  */
-if (!isVereniging() || !isAdmin()) {
+if (!isAdmin() && !isVereniging()) {
 	header("location:index.php");
 }
 $pagina = pagina::getInstantie();
 database::getInstantie();
-$id = ($_GET["id"]);
+
+
+if (isset($_POST["wijzigen"])) {
+	$error = array();
+	if (empty($_POST["naam"])) {
+		$error["naam"] = "U heeft geen naam ingevuld";
+	}
+	if (empty($_POST["bdatum"])) {
+		$error["bdatum"] = "U heeft geen begin datum ingevuld";
+	} else if (!tijd::checkCorrectieDatum($_POST["bdatum"])) {
+		$error["bdatum"] = "U heeft een onjuiste begin datum ingevuld";
+	}
+	if (!empty($_POST["edatum"]) && !tijd::checkCorrectieDatum($_POST["edatum"])) {
+		$error["edatum"] = "U heeft een onjuiste eind datum ingevuld";
+	}
+	if (empty($_POST["tekstvak"])) {
+		$error["tekstvak"] = "U heeft geen omschrijving ingevuld";
+	}
+	if (empty($_POST["categorie"])) {
+		$error["categorie"] = "U heeft geen categorie ingevuld";
+	}
+	if (empty($_POST["vereniging"])) {
+		$error["vereniging"] = "U heeft geen vereniging ingevuld";
+	}
+	if (empty($_POST["evenementid"])) {
+		$error["evenementid"] = "Er is een fout opgetreden";
+	}
+	
+	if (count(array_keys($error)) == 0) {
+		$naam = mysql_real_escape_string($_POST["naam"]);
+		$bdatum = mysql_real_escape_string(tijd::formatteerTijd($_POST["bdatum"], "Y-m-d"));
+		$edatum = mysql_real_escape_string(tijd::formatteerTijd($_POST["edatum"], "Y-m-d"));
+		$tekstvak = mysql_real_escape_string($_POST["tekstvak"]);
+		$categorie = mysql_real_escape_string($_POST["categorie"]);
+		$vereniging = mysql_real_escape_string($_POST["vereniging"]);
+		$evenement = mysql_real_escape_string($_POST["evenementid"]);
+		
+		if (isset($_POST["aanmelden"])) {
+			$aanmelden = "Ja";
+		} else {
+			$aanmelden = "Nee";
+		}
+		
+		$query = "UPDATE evenement 
+				SET categorieid='".$categorie."', naam='".$naam."', omschrijving='".$tekstvak."', begindatum='".$bdatum."', einddatum='".$edatum."', isaanmeldingverplicht='".$aanmelden."', organiserendeverenigingid='".$vereniging."' 
+				where evenementid=".$evenement.";";
+				mysql_query($query);
+	}
+}
+
+
+if (isset($_GET["id"])) {
+	$id = ($_GET["id"]);
+} else {
+	$id = null;
+}
+
 $sql = "SELECT * FROM `evenement` where evenementId = $id;";
 $resultaat_van_server = mysql_query($sql);
-$array = mysql_fetch_array($resultaat_van_server);
 
 $pagina->setTitel("Eventplaza");
 $pagina->setCss("style.css");
@@ -24,20 +79,25 @@ echo $pagina->getVereisteHTML();
 		<?php echo $pagina->getMenu(); ?>
 		<div id="content">
 			<h1><?php echo $pagina->getTitel(); ?></h1>
-			<form action="wijzigevenement2.php" method="post">
+			<?php
+			if ($resultaat_van_server) {
+				if (mysql_num_rows($resultaat_van_server) > 0) {
+					$array = mysql_fetch_array($resultaat_van_server);
+			?>
+			<form action="" method="post">
 				<table>
 					<tr>
 						<td>Naam </td>
-						<td><input type="text" name="naam" value="<?php echo $array["naam"]; ?>"/>
+						<td><input type="text" name="naam" value="<?php echo $array["naam"]; ?>"/><?php if (isset($error["naam"])) { echo "<td>".$error["naam"]."</td>"; } ?>
 						<input type="hidden" name="evenementid" value="<?php echo $id; ?>" /></td>
 					</tr>
 					<tr>
 						<td>Begin </td>	
-						<td><input type="text" name="bdatum" value="<?php echo tijd::formatteerTijd($array["begindatum"], "d-m-Y"); ?>"/><i>dd-mm-jjjj</i></td>
+						<td><input type="text" name="bdatum" value="<?php echo tijd::formatteerTijd($array["begindatum"], "d-m-Y"); ?>"/><?php if (isset($error["bdatum"])) { echo "<td>".$error["bdatum"]."</td>"; } else { ?><i>dd-mm-jjjj</i><?php } ?></td>
 					</tr>
 					<tr>
 						<td>Eind </td>
-						<td><input type="text" name="edatum" value="<?php echo tijd::formatteerTijd($array["einddatum"], "d-m-Y"); ?>"/><i>dd-mm-jjjj</i></td>
+						<td><input type="text" name="edatum" value="<?php echo tijd::formatteerTijd($array["einddatum"], "d-m-Y"); ?>"/><?php if (isset($error["edatum"])) { echo "<td>".$error["edatum"]."</td>"; } else { ?><i>dd-mm-jjjj</i><?php } ?></td>
 					</tr>
 					<tr>
 						<td>Categorie </td>
@@ -54,7 +114,7 @@ echo $pagina->getVereisteHTML();
 									echo "<option ".$selected." value=\"" . $categorie["categorieid"] . "\">" . $categorie["naam"] . "</option>";
 								}
 								?>
-							</select>
+							</select><?php if (isset($error["categorie"])) { echo "<td>".$error["categorie"]."</td>"; } ?>
 						</td>
 					</tr>
 					<tr>
@@ -71,7 +131,7 @@ echo $pagina->getVereisteHTML();
 									echo "<option ".$selected." value=\"" . $vereniging["verenigingid"] . "\">" . $vereniging["naam"] . "</option>";
 								}
 									?>							
-							</select>
+							</select><?php if (isset($error["vereniging"])) { echo "<td>".$error["vereniging"]."</td>"; } ?>
 						</td>
 					</tr>
 					<tr>
@@ -101,14 +161,22 @@ echo $pagina->getVereisteHTML();
 							$sql = "SELECT * FROM `Evenement` WHERE evenementId = $id;";
 							$resultaat_van_server = mysql_query($sql);
 							while ($array = mysql_fetch_array($resultaat_van_server)) { echo $array["omschrijving"];}
-							?></textarea>
+							?></textarea><?php if (isset($error["tekstvak"])) { echo "<td>".$error["tekstvak"]."</td>"; } ?>
 						</td>
 					</tr>
 					<tr>
-						<td><input type="submit" value="Opslaan"/></td>
+						<td colspan="2"><input type="submit" name="wijzigen" value="Opslaan"/><a href ="evenement.php?id=<?php echo $_GET["id"]; ?>">Terug</a></td>
 					</tr>
 				</table>
 			</form>
+			<?php
+				} else {
+					echo "Geen resultaten.";
+				}
+			} else {
+				echo "Geen resultaten.";
+			}
+			?>
 		</div>
 	</div>
 	<?php echo $pagina->getFooter(); ?>
